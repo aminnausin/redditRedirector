157bcd3e5f9c8e8ac61a5cbf574d802acf1a4b8c
aminnausin <aminnausin@outlook.com>
2024-08-13 17:50:45 -0400

    Do not redirect from images (/media pages)

--- redirect.js
+++ redirect.js
@@ -1,5 +1,6 @@
 const targetHostname = "new.reddit.com";
-if(window.location.hostname === 'www.reddit.com'){
+
+if(window.location.hostname === 'www.reddit.com' && window.location.pathname !== '/media'){
     let x = targetHostname + window.location.pathname;
     window.location.replace("https://" + x);
 }
